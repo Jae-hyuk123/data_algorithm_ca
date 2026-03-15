c224636dd4178111e0cf331013c756c1a7f60634
Jae-hyuk123 <wogur26300@gmail.com>
2026-03-15 15:13:03 +0000

    simple GUI

---
+++ smart_parking_app/src/main/java/com/mycompany/smart_parking_app/Smart_parking_app.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smart_parking_app;
+
+/**
+ *
+ * @author ABC
+ */
+public class Smart_parking_app {
+
+    public static void main(String[] args) {
+        SmartParkingAppGUI mygui = new SmartParkingAppGUI();
+        mygui.setVisible(true);
+    }
+}
